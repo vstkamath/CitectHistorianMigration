b207c6834e741652c3bb6a6306e3b461e3fc435b
vstkamath <35440665+vstkamath@users.noreply.github.com>
2019-07-16 00:08:32 -0700

    Add files via upload

--- MigrateCitectConfig.sql
+++ MigrateCitectConfig.sql
@@ -106,8 +106,19 @@ SELECT * INTO #CitectConfig
 FROM
 (
        SELECT 
-       T1.ID,T1.TagName, T1.Address, T1.DataTypeID, T1.DatasourceID, T1.Location, T1.Properties, T1.LastReadSampleTime, T2.PropertyName, T2.PropertyValue  
-       FROM  Tags T1,  TagProperties T2
+			T1.ID,
+			T1.TagName, 
+			T1.Address, 
+			T1.DataTypeID, 
+			T1.DatasourceID, 
+			T1.Location, 
+			T1.Properties, 
+			T1.LastReadSampleTime, 
+			T2.PropertyName, 
+			T2.PropertyValue  
+       FROM  
+			Tags T1,  
+			TagProperties T2
        WHERE T1.ID = T2.TagID
 ) AS SourceTable PIVOT(MIN(PropertyValue) FOR [PropertyName] IN(Comment, DataAcquisition, Eng_Full, Eng_Units, Eng_Zero, LoggingDeadBand, LoggingEnabled, Raw_Full, Raw_Zero)) AS PivotTable;
        
@@ -131,61 +142,118 @@ DECLARE     @MaxRaw NVARCHAR(256)
 -- Iterate over all Tag IDs
 WHILE (1 = 1) 
 BEGIN  
-  -- Get next TagID
-  SELECT TOP 1 @TagID = ID
-  FROM #CitectConfig
-  WHERE ID > @TagID 
-  ORDER BY ID
+	-- Get next TagID
+	SELECT TOP 1 
+		@TagID = ID
+	FROM 
+		#CitectConfig
+	WHERE 
+		ID > @TagID 
+	ORDER BY ID
 
-  -- Exit loop if no more Tags
-  IF @@ROWCOUNT = 0 BREAK;
+	-- Exit loop if no more Tags
+	IF @@ROWCOUNT = 0 BREAK;
 
-  -- Collect the data needed for Stored Proc
-SELECT @TagName = TagName, @Description = Properties, @DataSourceID = DatasourceID, @DataTypeID = DataTypeID,
-              @Location = Location, @MaxEu = Eng_Full, @MinEu = Eng_Zero, @EngineeringUnits = Eng_Units, @ValueDeaband = LoggingDeadband,
-              @MinRaw = Raw_Zero, @MaxRaw = Raw_Full
-FROM #CitectConfig 
- WHERE ID = @TagID   
+	-- Collect the data needed for Stored Proc
+	SELECT  @TagName = TagName, 
+			@Description = Properties, 
+			@DataSourceID = DatasourceID, 
+			@DataTypeID = DataTypeID,
+			@Location = Location, 
+			@MaxEu = Eng_Full, 
+			@MinEu = Eng_Zero, 
+			@EngineeringUnits = Eng_Units, 
+			@ValueDeaband = LoggingDeadband,
+			@MinRaw = Raw_Zero, @MaxRaw = Raw_Full
+	FROM #CitectConfig 
+	WHERE ID = @TagID   
  
-SELECT @DataSource = DatasourceName FROM Datasources WHERE ID = @DataSourceID
+	SELECT 
+		@DataSource = DatasourceName 
+	FROM Datasources 
+	WHERE ID = @DataSourceID
 
-IF NOT EXISTS( SELECT 1 FROM Runtime.dbo.EngineeringUnit WHERE Unit = @EngineeringUnits)
-BEGIN
-	EXEC Runtime.dbo.aaEngineeringUnitInsert   @EngineeringUnits, 1000, 1
-END
-SELECT @EUKey = EUKey FROM Runtime.dbo.EngineeringUnit WHERE Unit = @EngineeringUnits
+	IF NOT EXISTS( 
+		SELECT 1 
+		FROM Runtime.dbo.EngineeringUnit 
+		WHERE Unit = @EngineeringUnits)
+	BEGIN
+		EXEC Runtime.dbo.aaEngineeringUnitInsert   @EngineeringUnits, 1000, 1
+	END
 
-DECLARE @MnEU AS FLOAT(53) 
- SET @MnEU = CONVERT(FLOAT, @MinEu)
+	SELECT 
+		@EUKey = EUKey 
+	FROM Runtime.dbo.EngineeringUnit 
+	WHERE Unit = @EngineeringUnits
 
-DECLARE @MxEU AS FLOAT(53) 
- SET @MxEU = CONVERT(FLOAT, @MaxEU)
+	DECLARE @MnEU AS FLOAT(53) 
+	SET @MnEU = CONVERT(FLOAT, @MinEu)
 
-DECLARE @MnRAW AS FLOAT(53) 
- SET @MnRAW = CONVERT(FLOAT, @MinRAW)
+	DECLARE @MxEU AS FLOAT(53) 
+	SET @MxEU = CONVERT(FLOAT, @MaxEU)
 
-DECLARE @MxRAW AS FLOAT(53) 
- SET @MxRAW = CONVERT(FLOAT, @MaxRAW)
+	DECLARE @MnRAW AS FLOAT(53) 
+	SET @MnRAW = CONVERT(FLOAT, @MinRAW)
 
-DECLARE @VDeaband AS FLOAT(53) 
- SET @VDeaband = CONVERT(FLOAT, @ValueDeaband)
+	DECLARE @MxRAW AS FLOAT(53) 
+	SET @MxRAW = CONVERT(FLOAT, @MaxRAW)
 
-  -- call respectivesproc
-  IF @DataTypeID = 1
-  BEGIN
-       EXEC Runtime.dbo.aaAnalogTagInsert @TagName, @Description,2,2,0,N'',0,N'Citect',NULL,0,@EUKey, @MnEU,  @MxEU, @MnRaw , @MxRaw ,1,3,@VDeaband,0,16,0,NULL,NULL,1,0,0,0,254,0,0,1,NULL,NULL,0,NULL,1,NULL,NULL
-       --EXEC Runtime.dbo.aaAnalogTagInsert @TagName, @Description,1,2,0,N'',0,N'Citect',NULL,0,1,CAST(SELECT @MinEu as float), CAST(SELECT @MaxEu as float),CAST(SELECT @MinRaw as float),CAST(SELECT @MaxRaw as float),1,3,@ValueDeaband,0,16,0,NULL,NULL,1,0,0,0,254,0,0,1,NULL,NULL,0,NULL,1,NULL,NULL
-  END
+	DECLARE @VDeaband AS FLOAT(53) 
+	SET @VDeaband = CONVERT(FLOAT, @ValueDeaband)
 
-  IF @DataTypeID = 2
-  BEGIN
-       EXEC Runtime.dbo.aaDiscreteTagInsert @TagName, @Description,2,2,0,N'',0,N'Citect',NULL,0,1,0,NULL,NULL,0,0,0,1,NULL, NULL,0, NULL, 1, NULL, NULL
-  END
+	  -- call respectivesproc
+	  IF @DataTypeID = 1
+	  BEGIN
+		   EXEC Runtime.dbo.aaAnalogTagInsert 
+				@TagName, 
+				@Description,
+				2,
+				2,
+				0,
+				N'',
+				0,
+				N'Citect',
+				NULL,
+				0,
+				@EUKey,
+				@MnEU,
+				@MxEU,
+				@MnRaw,
+				@MxRaw,
+				1,
+				3,
+				@VDeaband,
+				0,
+				16,
+				0,
+				NULL,
+				NULL,
+				1,
+				0,
+				0,
+				0,
+				254,
+				0,
+				0,
+				1,
+				NULL,
+				NULL,
+				0,
+				NULL,
+				1,
+				NULL,
+				NULL
+	  END
 
-  IF @DataTypeID = 3
-  BEGIN
-       EXEC Runtime.dbo.aaStringTagInsert @TagName, @Description,2,2,0,N'',0,N'Citect',NULL,131,N'', NULL, NULL, 0,0, 0,0, 1,0, NULL, NULL, 0, NULL, 1, NULL, NULL , NULL
-  END
+	  IF @DataTypeID = 2
+	  BEGIN
+		   EXEC Runtime.dbo.aaDiscreteTagInsert @TagName, @Description,2,2,0,N'',0,N'Citect',NULL,0,1,0,NULL,NULL,0,0,0,1,NULL, NULL,0, NULL, 1, NULL, NULL
+	  END
+
+	  IF @DataTypeID = 3
+	  BEGIN
+		   EXEC Runtime.dbo.aaStringTagInsert @TagName, @Description,2,2,0,N'',0,N'Citect',NULL,131,N'', NULL, NULL, 0,0, 0,0, 1,0, NULL, NULL, 0, NULL, 1, NULL, NULL , NULL
+	  END
 
 END
 END
--- MigrateCitectData.sql
+++ MigrateCitectData.sql
@@ -77,7 +77,7 @@ BEGIN
 	DECLARE @OneDayOffset BIGINT;
 	DECLARE @TimeZone VARCHAR(50)
 	
-	EXEC MASTER.dbo.xp_regread 'HKEY_LOCAL_MACHINE',
+	EXEC MASTER.dbo.XP_Regread  'HKEY_LOCAL_MACHINE',
 								'SYSTEM\CurrentControlSet\Control\TimeZoneInformation',
 								'TimeZoneKeyName',
 								@TimeZone OUT
